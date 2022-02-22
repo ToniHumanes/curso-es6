f394c4826cd1691d7ea1915a3091599791e5382d
Antonio Humanes <antoniohumanespacheco@gmail.com>
2022-02-22 19:17:48 +0100

    Clase: ¿Qué se implementó en ES8?

---
+++ src/es8/index.js
@@ -0,0 +1,21 @@
+
+
+
+const data = {
+    front: 'Toni',
+    back: 'Antonio',
+    design: 'Alex'
+}
+
+const entries = Object.entries(data);
+// console.log(entries);
+// // para saber cuantos items tiene un object
+// console.log(entries.length)
+
+const values = Object.values(data);
+console.log(values)
+
+const string = 'Hellow';
+console.log(string.padStart(7, 'hi'));
+console.log(string.padEnd(12, '----'))
+
